5653b8d01987f544fa1cab5c79364af96a223c04
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-22 15:45:46 +1100

    done with the lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Article from "./Article";
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return <div className="App">learn react!
+  <Article />
+  </div>;
 }
 
 export default App;
